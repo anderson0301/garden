ba8be891cd448d9c6d276a47633feae5f63d8eed
anderson0301 <masamitsu.yano@gmail.com>
2018-02-28 15:04:16 +0900

    【更新】テンプレートファイルを整理

--- htdocs/wp-content/themes/original/ad-sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php $myAmp = false; $string = $post->post_content;if($_GET['amp'] === '1' && strpos($string,'<script>') === false && is_single()){$myAmp = true;}?>
-<?php if(!$myAmp): ?>
-<div class="ad-sidebar"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2374938888047560" data-ad-slot="6999020497" data-ad-format="rectangle"></ins>
-<script>(adsbygoogle = window.adsbygoogle||[]).push({});</script></div>
-<?php endif; ?>
